0e64f01fa8f49d55618a8a35cb5590b00d3af410
andreyamironov <40858348+andreyamironov@users.noreply.github.com>
2021-10-03 10:04:23 +0300

    UI_IdentityResources_CreateGet

---
+++ 6_AspNetIdentity/src/IdentityServerAspNetIdentity/Handlers/IdentityResources/CreateIdentityResourceGetHandler.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServerAspNetIdentity.Queries.IdentityResource;
+using IdentityServerAspNetIdentity.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdentityServerAspNetIdentity.Handlers.IdentityResources
+{
+    public class CreateIdentityResourceGetHandler : IRequestHandler<CreateIdentityResourceGetQuery, IdentityResourceCreateViewModel>
+    {
+        public Task<IdentityResourceCreateViewModel> Handle(CreateIdentityResourceGetQuery request, CancellationToken cancellationToken)
+        {
+            if (request.IdentityResourceViewModel == null) return Task.FromResult(new IdentityResourceCreateViewModel());
+            else return Task.FromResult(request.IdentityResourceViewModel);
+        }
+    }
+}
--- 6_AspNetIdentity/src/IdentityServerAspNetIdentity/ViewModels/IdentityResource/IdentityResourceCreateViewModel.cs
+++ 6_AspNetIdentity/src/IdentityServerAspNetIdentity/ViewModels/IdentityResource/IdentityResourceCreateViewModel.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace IdentityServerAspNetIdentity.ViewModels.IdentityResource
+namespace IdentityServerAspNetIdentity.ViewModels
 {
     public class IdentityResourceCreateViewModel : IdentityServer4.Models.IdentityResource
     {
+        public string ReturnUrl_VmProperty { get; set; }
     }
 }
